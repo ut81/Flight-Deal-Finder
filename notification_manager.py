4b70018ba7fe9b69185190cb49f764858c1c82f0
utagg <126648429+ut81@users.noreply.github.com>
2023-03-02 13:40:40 +0530

    Add files via upload

---
+++ notification_manager.py
@@ -0,0 +1,15 @@
+import smtplib
+
+my_email="YOUR EMAIL"
+passs="YOUR PASSWORD"
+
+class NotificationManager:
+    #This class is responsible for sending notifications with the deal flight details.
+    def send_emails(self,emails,message,google_flight_link):
+        
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email,passs)
+            for email in emails:
+                connection.sendmail(from_addr=my_email,to_addrs=email,msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8'))
+            
