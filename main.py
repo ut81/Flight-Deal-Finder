3b8a7d66380eb91a3641a957c354a703b38b9c90
utagg <126648429+ut81@users.noreply.github.com>
2023-03-02 15:16:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,7 @@ for destination_code in destinations:
 
     if flight.price < destinations[destination_code]["price"]:
 
-        users = data_manager.get_customer_emails()
-        emails = [row["email"] for row in users]
-        names = [row["firstName"] for row in users]
+        
 
         message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
 
@@ -52,6 +50,6 @@ for destination_code in destinations:
 
         link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
         
-        notification_manager.send_emails(emails, message, link)
+        notification_manager.send_emails(message, link)
 
 
